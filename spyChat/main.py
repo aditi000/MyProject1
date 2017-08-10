2902b427ec2d12d23ae23db5ccaab7d1f76a0de8
aditi000 <aditisharma91912@gmail.com>
2017-08-11 00:40:40 +0530

    Spychat 1st file

---
+++ spyChat/main.py
@@ -0,0 +1,5 @@
+print 'Let\'s get Started'
+spy_salutation=raw_input('Hey! What should we call you(Mr./Mrs/Miss)?')
+spy_name=raw_input('May I get your name,dear?')
+if len(spy_name)>0:
+    print'Welcome, '+spy_salutation+' '+spy_name+'. We are glad to have you back with us'
